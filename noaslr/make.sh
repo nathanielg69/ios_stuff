070f81ec80089f4b4895ca7699231fb085681259
3x7R00Tripper <yoda.kremer@gmail.com>
2013-12-04 21:08:28 +0100

    Create make.sh
    Read ../README.md first

---
+++ noaslr/make.sh
@@ -0,0 +1,14 @@
+#
+# This make script was created by Louis Kremer aka. @3x7R00Tripper.
+#
+
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "make.sh must be run as root" 2>&1
+  exit 1
+else
+  gcc -o noaslr main.c
+  sudo cp noaslr /sw/bin/
+  echo "Installed successfully"
+fi
